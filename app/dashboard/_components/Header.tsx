0bd9748bd1c82beda6ba0afa172069a5b38d369b
shahi21 <shahisthanavab16@gmail.com>
2025-06-19 15:46:42 +0530

    removed header search

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -6,9 +6,9 @@ function Header() {
   return (
     <div className='p-5 shadow-sm border-b-2 bg-white flex justify-between items-center'>
         <div className='flex gap-2 items-center p-2 border rounded-md max-w-lg bg-white'>
-            <Search />
-            <input type="text" placeholder='Search ...' 
-            className='outline-none '/>
+            {/* <Search /> */}
+            {/* <input type="text" placeholder='Search ...' 
+            className='outline-none '/> */}
         </div>
 
         <div className='flex gap-2 items-center'>
